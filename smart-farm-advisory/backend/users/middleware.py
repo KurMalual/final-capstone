b7e5009ab3e29f0a68e937ab758b1658dcd7cd13
KurMalual <k.malual@alustudent.com>
2025-06-29 00:15:50 +0200

    done

--- smart-farm-advisory/backend/users/middleware.py
+++
@@ -1,7 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-class CSRFExemptMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        # Exempt API endpoints from CSRF
-        if request.path.startswith('/api/auth/'):
-            setattr(request, '_dont_enforce_csrf_checks', True)
