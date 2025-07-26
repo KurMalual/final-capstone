f0583983782e1c4fa246e16416e4b87a7e93f4ce
KurMalual <k.malual@alustudent.com>
2025-07-26 16:24:53 +0200

    Done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,12 +47,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'backend.equipment',
-    'backend.transport',
-    'backend.marketplace',
-    'backend.users',
-    'backend.education',
-    'backend.weather',
+    'equipment',
+    'transport',
+    'marketplace',
+    'users',
+    'education',
+    'weather',
 ]
 
 MIDDLEWARE = [
