76dfb5d78c03e27215e102f40f7271c695344b3d
KurMalual <k.malual@alustudent.com>
2025-07-26 16:08:03 +0200

    done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
-    'equipment',
+    'backend.equipment',
     'transport',
     'marketplace',
     'users',
