6c83c09d59bab613156f6d60955174954b462b64
KurMalual <k.malual@alustudent.com>
2025-07-27 00:52:34 +0200

    done

--- backend/smartfarm_backend/wsgi.py
+++ backend/smartfarm_backend/wsgi.py
@@ -5,7 +5,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+# Print the PYTHONPATH and sys.path for debugging
+print("PYTHONPATH:", sys.path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smartfarm_backend.settings")
 
