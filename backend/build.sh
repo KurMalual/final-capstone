51c664097d324261281952b591fa59ae1132fc87
KurMalual <k.malual@alustudent.com>
2025-07-26 22:40:22 +0200

    done

--- backend/build.sh
+++ backend/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
+
+# Apply database migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
