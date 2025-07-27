cfb2420784983e1cfcbb2d682d549275ccc235a9
KurMalual <k.malual@alustudent.com>
2025-07-27 02:20:45 +0200

    done

--- backend/smartfarm_backend/wsgi.py
+++ backend/smartfarm_backend/wsgi.py
@@ -7,6 +7,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
 sys.path.append(str(BASE_DIR.parent))
 
+# Debugging: Print sys.path to verify paths
+print("sys.path:", sys.path)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartfarm_backend.settings')
